83e46977d1490e07d08abf5059da8a4c83e1fc6a
ozubergz <jmin499@gmail.com>
2019-12-11 15:48:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(json => { 
+      renderBooks(json)
+    });
 }
 
+//render books
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
-  })
+  });
 }
 
+//call function when DOM is loaded
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+  fetchBooks();
+});
